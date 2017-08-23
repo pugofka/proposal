09f147599f3070510d47fec4b6ef70e51da8ebb0
Olga Bolotova <o.bolotova@pugofka.com>
2017-08-23 17:13:01 +0300

    add stages page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,12 +70,14 @@
                 </div>
             </div>
         </nav>
+        <nav class="sidebar">
         <ul class="nav nav-pills nav-stacked">
           <li class="active"><a href="javascript:void(0)">Этапы</a></li>
           <li><a href="javascript:void(0)">Выбор шаблона</a></li>
           <li><a href="javascript:void(0)">Вопросы</a></li>
           <li><a href="javascript:void(0)">Настройки</a></li>
         </ul>
+        </nav>
 
         @yield('content')
     </div>
