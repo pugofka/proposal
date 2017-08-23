31a014ac923c212d20d59885dc6af0dac2d66c5c
Olga Bolotova <o.bolotova@pugofka.com>
2017-08-23 17:47:16 +0300

    add create page

--- resources/views/stages/create.blade.php
+++ resources/views/stages/create.blade.php
@@ -1,1 +1,40 @@
-123
+@extends('layouts.app')
+
+@section('content')
+<section>
+<form class="form-horizontal">
+  <fieldset>
+    <legend>Создание этапа</legend>
+    <div class="form-group">
+      <label for="name" class="col-md-2 control-label">Название этапа</label>
+
+      <div class="col-md-10">
+        <input type="text" class="form-control" id="name">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="number" class="col-md-2 control-label">Номер этапа</label>
+
+      <div class="col-md-10">
+        <input type="number" class="form-control" id="number">
+
+        <div class="togglebutton">
+          <label>
+            <input type="checkbox" checked> Активность этапа
+          </label>
+        </div>
+        
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <div class="col-md-10 col-md-offset-2">
+        <button type="button" class="btn btn-default">Удалить</button>
+        <button type="submit" class="btn btn-primary">Создать</button>
+      </div>
+    </div>
+  </fieldset>
+</form>
+</section>
+
+@endsection
