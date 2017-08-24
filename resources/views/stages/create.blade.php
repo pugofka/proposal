7707437244d38e577e5bc3fa1a4e547e84e31b13
Olga Bolotova <o.bolotova@pugofka.com>
2017-08-24 15:14:34 +0300

    fix questions page

--- resources/views/stages/create.blade.php
+++ resources/views/stages/create.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<section>
-<form class="form-horizontal">
+<section class="create">
+<form class="create__form form-horizontal">
   <fieldset>
     <legend>Создание этапа</legend>
     <div class="form-group">
-      <label for="name" class="col-md-2 control-label">Название этапа</label>
+      <label for="name" class="col-md-3 control-label">Название этапа</label>
 
-      <div class="col-md-10">
+      <div class="col-md-9">
         <input type="text" class="form-control" id="name">
       </div>
     </div>
     <div class="form-group">
-      <label for="number" class="col-md-2 control-label">Номер этапа</label>
+      <label for="number" class="col-md-3 control-label">Сортировка</label>
 
-      <div class="col-md-10">
+      <div class="col-md-9">
         <input type="number" class="form-control" id="number">
 
         <div class="togglebutton">
