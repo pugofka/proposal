8508477a942b3a98350b8f8108ab2b49bbf8d9a4
Валера Любимов <v.liubimov@pugofka.com>
2017-09-07 13:56:05 +0300

    Add steps

--- resources/assets/js/components/calculation/Index.vue
+++ resources/assets/js/components/calculation/Index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    calculation
+  <steps
+    :stepsData="steps"
+  />
   </div>
 </template>
 
 <script>
   export default {
+    components: {
+      'steps': require('./Steps.vue')
+    },
+
+    data: function () {
+      return {
+        steps: {
+          current: 0,
+          list: [
+            'Начало',
+            'Задачи',
+            'Дополнительные задачи',
+            'Расходы клиента',
+            'Результат'
+          ]
+        }
+      }
+    }
 
   }
 </script>
