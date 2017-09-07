efa62b4a8ec5619ca366a48a78ae1c7e211e9ae2
Валера Любимов <v.liubimov@pugofka.com>
2017-09-07 12:58:31 +0300

    Start feature

---
+++ resources/assets/js/components/calculation/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    calculation
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
---
+++ resources/views/calculation/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Создание расчета</h2>
+    <calculation/>
+@endsection
