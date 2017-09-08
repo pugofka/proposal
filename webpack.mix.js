d1f7fc9281f502f2db0dc51bac7c40c3e7190c59
Валера Любимов <v.liubimov@pugofka.com>
2017-09-08 14:29:18 +0300

    Update webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,15 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.browserSync('localhost');
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .extract(['vue'])
+   .sourceMaps();
+
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+   .sourceMaps();
+
+if (mix.inProduction()) {
+  mix.version();
+}
