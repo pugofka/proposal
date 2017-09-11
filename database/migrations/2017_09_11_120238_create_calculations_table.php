ed862112436b4f93cb3d100339fd2e98fff7f79c
Mirbabaev Maksim <m.mirbabaev@pugofka.com>
2017-09-11 15:09:32 +0300

    new model calculation added want to start pull and merge frontend calculation_page

---
+++ database/migrations/2017_09_11_120238_create_calculations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalculationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calculations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('user_name');
+            $table->string('user_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calculations');
+    }
+}
