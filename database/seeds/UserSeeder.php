65e24c09a1891233a60d7288ad3b310d2fd567f7
Mirbabaev Maksim <m.mirbabaev@pugofka.com>
2017-09-12 17:19:48 +0300

    UserSeeder added

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+
+        \App\User::create([
+            'name' => 'admin',
+            'email' => 'email@domain.ru',
+            'password' => Hash::make('secret'),
+        ]);
+    }
+}
