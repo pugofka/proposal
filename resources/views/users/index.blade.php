93f54557636a0efbd26a34ccc145c5f30742bdb1
Mirbabaev Maksim <m.mirbabaev@pugofka.com>
2017-09-13 18:40:52 +0300

    some fixes at account page form

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -22,6 +22,9 @@
       <div class="input-group">
         <span class="input-group-addon users__field-name" id="basic-addon1">Пароль</span>
         {{ Form::password('user_new_password', ['class' => 'form-control users__input']) }}
+      </div>
+      <div class="input-group">
+        <span class="input-group-addon users__field-name" id="basic-addon1">Подтверждение пароля</span>
         {{ Form::password('user_new_password_confirm', ['class' => 'form-control users__input']) }}
       </div>
       {{ Form::submit('Обновить данные', ['class' => 'btn btn-primary']) }}
