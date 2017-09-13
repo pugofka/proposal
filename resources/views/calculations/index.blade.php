818458907e85eece1a72532526006867f3bf43b2
Mirbabaev Maksim <m.mirbabaev@pugofka.com>
2017-09-13 17:58:50 +0300

    changed new calculation btn style

--- resources/views/calculations/index.blade.php
+++ resources/views/calculations/index.blade.php
@@ -2,5 +2,9 @@
 
 @section('content')
   <h2>Все расчёты</h2>
-  <a href="{{ route('calculations.create') }}" class="calculations__new-calculation-link">Ноывый расчёт</a>
+  <a href="{{ route('calculations.create') }}" class="calculations__new-calculation-link stages__btn btn btn-warning btn-fab">
+    <i class="material-icons">
+      <img src="{{ URL::asset('/img/plus.svg') }}" alt="">
+    </i>
+  </a>
 @endsection
