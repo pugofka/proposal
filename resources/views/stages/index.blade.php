2e32251f23c696d0b5c07889240a3ed4f05cb2b6
Валера Любимов <v.liubimov@pugofka.com>
2017-09-15 19:13:05 +0300

    Refactor stages list

--- resources/views/stages/index.blade.php
+++ resources/views/stages/index.blade.php
@@ -1,41 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="stages">
-    <div class="row">
-      <div class="stages__wrap">
-        <h1 class="stages__title">Этапы</h1>
-        <a href="{{ route('stages.create') }}" class="stages__btn btn btn-warning btn-fab">
-          <i class="material-icons">
-            <img src="{{ URL::asset('/img/plus.svg') }}" alt="">
-          </i>
-        </a>
-      </div>
-      <table class="table table-striped table-hover stages__table">
-        <thead>
-        <tr>
-          <th>Название этапа</th>
-          <th>Редактирование</th>
-        </tr>
-        </thead>
-        <tbody>
+    <section>
+        <div class="row">
+            <h1>Этапы</h1>
 
-        @foreach($stages as $stage)
-          <tr>
-            <td>{{ $stage->name }}</td>
-            <td>
-              <a href="{{ route('stages.edit', ['íd' => $stage->id]) }}" class="stages__modify-btn btn btn-warning btn-fab">
-                <i class="material-icons">
-                  <img src ="{{URL::asset('/img/pen.svg')}}" alt="">
-                </i>
-              </a>
-            </td>
-          </tr>
+            @if(isset($stages) && !$stages->isEmpty())
+                @component('components.table-list', ['list' => $stages, 'type' => 'stages'])@endcomponent
+            @else
+                @component('components.alert')
+                    @slot('type') info @endslot
+                    @slot('title') Нет данных @endslot
+                    Не найдено ни одного этапа!
+                @endcomponent
+            @endif
 
-        @endforeach
-        </tbody>
-      </table>
-    </div>
-  </section>
-
+            @component('components.add-btn') stages @endcomponent
+        </div>
+    </section>
 @endsection
